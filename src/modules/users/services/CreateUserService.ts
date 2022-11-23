3068e970df7b25ec490a061483ceef36da5b92dc
Alex Gomes <alexg.marinhoo@gmail.com>
2022-11-23 16:47:48 -0300

    hashedPassword

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,13 +1,13 @@
 import { User } from "@prisma/client";
 import { AppError } from "../../../shared/errors/AppError";
 import { prismaClient } from "../../../databases/prismaClient";
+import { hash } from "bcryptjs";
 
 interface IRequest {
   name: string;
   email: string;
   password: string;
 }
-
 
 export class CreateUserService {
   async execute({ name, email, password }: IRequest): Promise<User> {
@@ -17,11 +17,13 @@ export class CreateUserService {
 
     if (emailExists) throw new AppError("Email address already used");
 
+    const hashedPassword = await hash(password, 8);
+
     const user = await prismaClient.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     });
 
